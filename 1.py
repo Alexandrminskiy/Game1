9ec965b8462daa69752542ac4bf7ed3eee450beb
alexandrminskiy <alexandrminskiy@gmail.com>
2022-02-21 12:59:22 +0300

    img player

--- 1.py
+++ 1.py
@@ -1,7 +1,13 @@
 import pygame as pg
 import sys
+import os
+
+# Настройка папки ассетов
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, 'img')
+player_img = pg.image.load(os.path.join(img_folder, 'p1_jump.png'))
+
 # здесь определяются константы,
-# классы и функции
 FPS = 60
 WIN_WIDTH = 600
 WIN_HEIGHT = 700
@@ -12,19 +18,19 @@ RIGHT = "RIGHT"
 LEFT = "LEFT"
 DOWN = "DOWN"
 UP = "UP"
+motion = STOP
 # здесь происходит инициация,
 # создание объектов
 pg.init()
 pg.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
 clock = pg.time.Clock()
 sc = pg.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
-sc.fill(GREEN)
 
+# классы и функции
 class Player(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((50,50))
-        self.image.fill(BROWN)
+        self.image = player_img
         self.rect = self.image.get_rect()
         self.rect.center = (WIN_WIDTH / 2, WIN_HEIGHT / 2)
 
@@ -38,16 +44,11 @@ class Player(pg.sprite.Sprite):
         elif motion == DOWN:
             self.rect.y += 5
 
-
 all_sprites = pg.sprite.Group()
 player = Player()
 all_sprites.add(player)
+# pg.display.update()
 
-x = 250
-y = 300
-
-pg.display.update()
-motion = STOP
 while 1:
     clock.tick(FPS)
     for i in pg.event.get():
@@ -70,10 +71,5 @@ while 1:
     all_sprites.update()
     sc.fill(GREEN)
     all_sprites.draw(sc)
-    # pg.draw.rect(sc, BROWN, (x,y,50,50))
     pg.display.update()
-    # if motion == LEFT: x -= 5
-    # elif motion == RIGHT: x += 5
-    # elif motion == UP: y -= 5
-    # elif motion == DOWN: y += 5
     clock.tick(FPS)
